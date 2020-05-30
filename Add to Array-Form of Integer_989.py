bf35ba53a6236e870bebc5aa48c09e9bccaca20b
Pauravi Wagh <pauravi.wagh12@gmail.com>
2020-05-30 12:57:32 -0400

    1089

---
+++ Add to Array-Form of Integer_989.py
@@ -0,0 +1,18 @@
+
+A = [2,0,6]
+K = 806
+
+i = len(A) - 1
+while i >= 0 and K > 0:
+    _sum = A[i] + K
+    A[i] = _sum%10
+    K =  _sum// 10
+    i -= 1
+print(A, _sum)
+while K>0:
+    A.insert(0, K%10)
+    K = K//10
+print(A)
+
+
+
--- Maximize Distance_849.py
+++ Maximize Distance_849.py
@@ -18,49 +18,19 @@ count = 0
 #         count = 0
 #
 #
-# l = [1,1,0,0,0,1,0]
-l = [1,0,0,0]
-l = [0,1,1,1,0,0,1,0,0]
-for i, data in enumerate(l):
-    if data == 0:
-        count += 1
+l = [1,1,0,0,0,1,0]
+import re
+# l = [1,0,0,0,1,0,1]
+# l = [1,1,1,0,1,0,1,1,1,1,1,1,1,1,1]
+# l = [1,0,0,0]
+seats = [0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,1]
+consequentZero, ans = 0, 0
+for s in seats:
+    if s == 0:
+        consequentZero += 1
+        ans = max(ans, consequentZero)
+    else:
+        consequentZero = 0
+ans = (ans + 1) // 2
 
-        if count > max_zeros:
-            max_zeros = count
-            start = i - max_zeros + 1
-    if data == 1:
-        if count > max_zeros:
-            max_zeros = count
-            start = i - max_zeros + 1
-        count = 0
-
-
-if start == 0:
-    index = start
-elif start + max_zeros == len(l):
-    index = len(l) - 1
-else:
-    index = start + (max_zeros)//2
-dist1 = 0
-
-
-# l = [1,1,0,0,0,1,0]
-# [0,1,1,1,0,0,1,0,0]
-print(max_zeros, start, len(l), index)
-for i in range(index+1, len(l)):
-    print(i, l[i],dist1, "order")
-    if  l[i] == 0:
-        dist1 +=1
-    if l[i] == 1:
-        dist1 += 1
-        break
-dist2 = 0
-for i in reversed(range(0,index)):
-
-    if l[i] == 0:
-        dist2 += 1
-    if l[i] == 1:
-        dist2 += 1
-        break
-print(dist1, dist2)
-    # return start
+print(max(ans, seats.index(1), seats[::-1].index(1)))
