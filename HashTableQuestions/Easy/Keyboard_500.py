0ca94a6f0a8e8261e44e5ab592f0d4723add536d
Pauravi Wagh <pauravi.wagh12@gmail.com>
2020-06-27 17:29:40 -0400

    reshuffling

--- HashTableQuestions/Easy/Keyboard_500.py
+++
@@ -1,9 +0,0 @@
-words = ["Hello", "Alaska", "Dad", "Peace"]
-
-x = []
-k = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
-for ele in words:
-    for i in k:
-        if set(ele.lower()).issubset(i):
-            x.append(ele)
-print(x)
