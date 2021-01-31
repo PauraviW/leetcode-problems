5adba090b4dc6dc75aa8702528a16b1cf5c0e4f4
pauravi <pauravi.wagh12@gmail.com>
2021-01-31 15:53:53 -0500

    five-star-sellers

--- Amazon SDE 1/five-star-sellers.py
+++ Amazon SDE 1/five-star-sellers.py
@@ -4,22 +4,28 @@ def fiveStartReviews(productRatings, ratingsThreshold):
     result = 0
     reqdSum = ratingsThreshold * totalProducts * 1.0 / 100
     currentThreshold = 0
+
     for i in range(len(productRatings)):
         currentThreshold += productRatings[i][0] / productRatings[i][1]
+
     currentThreshold /= totalProducts
     if currentThreshold * 100 >= ratingsThreshold:
         return 0
     currentSum = 0
+    zero_contribution = set()
     while currentSum < reqdSum:
         currentSum = 0
         maxContribution = 0
         productItem = -1
         for i in range(len(productRatings)):
-            contribution = (productRatings[i][0] + 1) * 1.0 / (productRatings[i][1] + 1) - productRatings[i][0] * 1.0 / \
-                           productRatings[i][1]
-            if maxContribution < contribution:
-                maxContribution = contribution
-                productItem = i
+            if i not in zero_contribution:
+                contribution = (productRatings[i][0] + 1) * 1.0 / (productRatings[i][1] + 1) - productRatings[i][0] * 1.0 / \
+                               productRatings[i][1]
+                if contribution == 0:
+                    zero_contribution.add(i)
+                if maxContribution < contribution:
+                    maxContribution = contribution
+                    productItem = i
 
             currentSum += productRatings[i][0] * 1.0 / productRatings[i][1]
 
@@ -30,3 +36,9 @@ def fiveStartReviews(productRatings, ratingsThreshold):
         result += 1
     return result
 
+print(fiveStartReviews([[4,4], [2,3], [3, 6]], 77))
+
+"""
+In this question, what I am trying to do is to find out how much more increase in the threshold is needed and choose amongst the items which item has the highest percentage of contribution.
+
+"""
