2fd8fd110b88583f800cc50c3d2a9fda6b10826d
pauravi <pauravi.wagh12@gmail.com>
2021-01-31 17:46:16 -0500

    utilization Timers

--- Amazon SDE 1/UtilizationTimer.py
+++ Amazon SDE 1/UtilizationTimer.py
@@ -12,7 +12,7 @@ def finalInstances(instances, averageUtil):
     timer = False
 
     while i < len(averageUtil):
-
+        #
         if averageUtil[i] < 25:
             if instances > 1:
                 instances = math.ceil(instances / 2)
@@ -32,3 +32,14 @@ def finalInstances(instances, averageUtil):
 # print(finalInstances(2, [25, 23, 1, 2, 3, 4 ,  5 ,  6 ,  7 ,  8 ,  9 ,  10 ,  76 ,  80 ]))
 print(finalInstances(1, [5 , 10, 80 ]))
 print(finalInstances(100000000, [  30  ,   95  ,   4  ,   8  ,   19  ,   89  ]))
+
+# Time Complexity - O(n)
+# Space Complexity - O(1)
+
+
+"""
+In this question, I go through every time stamp and check for the load at each instance. IF the load is below 25, i check if the number of instances is greater than one and then only divide the instances
+else if the load is above 75 i check if doubling the instances does not cause an overflow and accordingly change it. 
+I use a timer flag to track if we need to check the next instance or not. IF a timer flag is added, then i skip the next 10 values else i chekc all the instances. Toggling of flag is handled accordingly. 
+
+"""
